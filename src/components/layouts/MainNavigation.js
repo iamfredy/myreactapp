4c4499991a39c9ae79bd118fd5388bc4a2a219f6
Fred <Fredy799@gmail.com>
2021-11-03 22:16:16 +0530

    Navigation Bar modified with bootstrap

--- src/components/layouts/MainNavigation.js
+++ src/components/layouts/MainNavigation.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import { Nav } from 'react-bootstrap';
+
 
 export default function MainNavigation() {
     return (
         <div>
-            <h1>My Webpage</h1>
-            <ul>
-                <li><Link to="/">All Meetup</Link></li>
-                <li><Link to="/NewMeetup">New Meetup</Link></li>
-                <li><Link to="/Favourites">Favourite</Link></li>  
-            </ul>          
+            <h1 className="h1">My Webpage</h1>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand> <Link className="navbar-brand" to="/">Home</Link></Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link><Link className="navbar-brand" to="/AllMeetUps">All Meetups</Link></Nav.Link>
+                    <Nav.Link><Link className="navbar-brand" to="/NewMeetup">New Meetup</Link></Nav.Link>
+                    <Nav.Link><Link className="navbar-brand" to="/Favourites">Favourite</Link></Nav.Link>                    
+                </Nav>
+            </Navbar>
         </div>
     )
 }
