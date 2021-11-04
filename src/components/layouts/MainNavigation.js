25b32b7a855c685cc55c1d018f4fab867ad5d13e
Fred <Fredy799@gmail.com>
2021-11-04 10:36:32 +0530

    CSS Modules - Added Separate CSS Module(File) for Nav Bar

--- src/components/layouts/MainNavigation.js
+++ src/components/layouts/MainNavigation.js
@@ -3,18 +3,19 @@ import { Link } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
 import { Nav } from 'react-bootstrap';
+import cssClasses from './MainNavigation.module.css';
 
 
 export default function MainNavigation() {
     return (
-        <div>
+        <div className={cssClasses.header}>
             <h1 className="h1">My Webpage</h1>
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand> <Link className="navbar-brand" to="/">Home</Link></Navbar.Brand>
+            <Navbar expand="lg">
+                <Navbar.Brand> <Link className={cssClasses.nav+" navbar-brand"} to="/">Home</Link></Navbar.Brand>
                 <Nav className="me-auto">
-                    <Nav.Link><Link className="navbar-brand" to="/AllMeetUps">All Meetups</Link></Nav.Link>
-                    <Nav.Link><Link className="navbar-brand" to="/NewMeetup">New Meetup</Link></Nav.Link>
-                    <Nav.Link><Link className="navbar-brand" to="/Favourites">Favourite</Link></Nav.Link>                    
+                    <Nav.Link><Link className={cssClasses.nav+" navbar-brand"} to="/AllMeetUps">All Meetups</Link></Nav.Link>
+                    <Nav.Link><Link className={cssClasses.nav+" navbar-brand"} to="/NewMeetup">New Meetup</Link></Nav.Link>
+                    <Nav.Link><Link className={cssClasses.nav+" navbar-brand"} to="/Favourites">Favourite</Link></Nav.Link>                    
                 </Nav>
             </Navbar>
         </div>
